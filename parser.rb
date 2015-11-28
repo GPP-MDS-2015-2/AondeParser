0c27727bb0e9a2ab5e51076a02526eef5b78da09
Gabriel Silva <gabriel93.silva@gmail.com>
2015-11-28 14:59:11 -0200

    Changing to Mysql2 gem

--- parser.rb
+++ parser.rb
@@ -1,33 +1,39 @@
 require 'csv'
-require 'mysql'
+require 'mysql2'
 
 
-file_name = ARGV[0]
-file_size = `wc -l #{file_name}`.to_f
+db_name = "aonde_parser"
+folder_name = ARGV[0]
+folder = Dir.glob("#{folder_name}/*.csv")
 
 
-def create_tables(con)
-	con.query("CREATE TABLE IF NOT EXISTS \
+def init_database(con, db_name)
+    con.query("DROP DATABASE IF EXISTS #{db_name}")
+    con.query("CREATE DATABASE #{db_name}")
+    con.query("USE #{db_name}")
+    con.select_db(db_name)
+
+    con.query("CREATE TABLE IF NOT EXISTS \
         superior_public_agencies(id INTEGER(11) PRIMARY KEY, name VARCHAR(50))")
-	con.query("CREATE TABLE IF NOT EXISTS \
+    con.query("CREATE TABLE IF NOT EXISTS \
         public_agencies(id INTEGER(11) PRIMARY KEY, name VARCHAR(50), \
         superior_public_agency_id INTEGER(11), views_amount INT DEFAULT 0, CONSTRAINT superior_public_agencies_public_agencies \
-		FOREIGN KEY(superior_public_agency_id) REFERENCES superior_public_agencies(id))")
-	con.query("CREATE TABLE IF NOT EXISTS \
+    	FOREIGN KEY(superior_public_agency_id) REFERENCES superior_public_agencies(id))")
+    con.query("CREATE TABLE IF NOT EXISTS \
         programs(id INTEGER(11) PRIMARY KEY, name VARCHAR(100), description VARCHAR(100))")
-	con.query("CREATE TABLE IF NOT EXISTS \
+    con.query("CREATE TABLE IF NOT EXISTS \
         type_expenses(id INTEGER(11) PRIMARY KEY, description VARCHAR(50))")
-	con.query("CREATE TABLE IF NOT EXISTS \
+    con.query("CREATE TABLE IF NOT EXISTS \
         companies(id INTEGER(11) PRIMARY KEY, name VARCHAR(50))")
-	con.query("CREATE TABLE IF NOT EXISTS \
+    con.query("CREATE TABLE IF NOT EXISTS \
         functions(id INTEGER(11) PRIMARY KEY, description VARCHAR(50))")
-	con.query("CREATE TABLE IF NOT EXISTS \
-        expenses(id INT PRIMARY KEY AUTO_INCREMENT, document_number VARCHAR(15), \
+    con.query("CREATE TABLE IF NOT EXISTS \
+        expenses(document_number VARCHAR(15), \
         payment_date Date NOT NULL, value NUMERIC(13,2), public_agency_id INTEGER(11), program_id INTEGER(11), type_expense_id INTEGER(11), company_id INTEGER(11), function_id INTEGER(11), \
         CONSTRAINT public_agencies_programs FOREIGN KEY(public_agency_id) REFERENCES public_agencies(id), \
         CONSTRAINT programs_expenses FOREIGN KEY(program_id) REFERENCES programs(id), \
-		CONSTRAINT type_expenses_expenses FOREIGN KEY(type_expense_id) REFERENCES type_expenses(id), \
-		CONSTRAINT functions_expenses FOREIGN KEY(function_id) REFERENCES functions(id))")
+    	CONSTRAINT type_expenses_expenses FOREIGN KEY(type_expense_id) REFERENCES type_expenses(id), \
+    	CONSTRAINT functions_expenses FOREIGN KEY(function_id) REFERENCES functions(id))")
 end
 
 def create_preprocess_data_table(con)
@@ -38,39 +44,48 @@ def create_preprocess_data_table(con)
 
 	con.query("INSERT INTO public_agency_graph (id_public_agency, year, value) \
 		SELECT public_agency_id, EXTRACT(YEAR FROM payment_date), SUM(value) \
-		FROM expenses \
-		GROUP BY public_agency_id, EXTRACT(YEAR FROM payment_date)")
+		FROM expenses GROUP BY public_agency_id, EXTRACT(YEAR FROM payment_date)")
 end
 
+total_files = folder.count
+current_file = 1
 
-begin
-    con = Mysql.new 'localhost', 'root', 'root', 'aonde_parser'    
-    create_tables(con)
+folder.each do |csv_file|
+    begin
+        con = Mysql2::Client.new(hostname: 'localhost', user: 'root', password: 'root')
+        init_database(con, db_name)
+        file_size = `wc -l #{csv_file}`.to_f
+        count = 0
 
-    count = 0
-
-	CSV.foreach(file_name, encoding: "iso-8859-1:utf-8",
-			headers: true, col_sep: "\t", quote_char: "|") do |row|
-		con.query("INSERT IGNORE INTO superior_public_agencies(id, name) VALUES(\"#{row["Código Órgão Superior"]}\", \"#{row["Nome Órgão Superior"]}\")")
-		con.query("INSERT IGNORE INTO public_agencies(id, name, superior_public_agency_id) VALUES(\"#{row["Código Órgão"]}\", \"#{row["Nome Órgao"]}\", \"#{row["Código Órgão Superior"]}\")")
-		con.query("INSERT IGNORE INTO programs(id, name, description) VALUES(\"#{row["Código Programa"]}\", \"#{row["Nome Programa"]}\", \"#{row["Linguagem Cidadã"]}\")")
-		con.query("INSERT IGNORE INTO type_expenses(id, description) VALUES(\"#{row["Código Elemento Despesa"]}\", \"#{row["Nome Elemento Despesa"]}\")")
-		con.query("INSERT IGNORE INTO companies(id, name) VALUES(\"#{row["Código Favorecido"]}\", \"#{row["Nome Favorecido"].gsub("\"", "\'")}\")")
-		con.query("INSERT IGNORE INTO functions(id, description) VALUES(\"#{row["Código Função"]}\", \"#{row["Nome Função"]}\")")
-		con.query("INSERT IGNORE INTO expenses(public_agency_id, document_number, value, payment_date, program_id, function_id, type_expense_id, company_id) \
-			VALUES(\"#{row["Código Órgão"]}\", \"#{row["Número Documento"]}\", \"#{row["Valor"]}\", STR_TO_DATE('#{row["Data Pagamento"]}', '%d/%m/%Y'), \"#{row["Código Programa"]}\", \"#{row["Código Função"]}\", \"#{row["Código Elemento Despesa"]}\", \"#{row["Código Favorecido"]}\")")
+    	CSV.foreach(csv_file, encoding: "iso-8859-1:utf-8",
+    			headers: true, col_sep: "\t", quote_char: "|") do |row|
+    		con.query("INSERT IGNORE INTO superior_public_agencies(id, name) VALUES(\"#{row["Código Órgão Superior"]}\", \"#{row["Nome Órgão Superior"]}\")")
+    		con.query("INSERT IGNORE INTO public_agencies(id, name, superior_public_agency_id) VALUES(\"#{row["Código Órgão"]}\", \"#{row["Nome Órgao"]}\", \"#{row["Código Órgão Superior"]}\")")
+    		con.query("INSERT IGNORE INTO programs(id, name, description) VALUES(\"#{row["Código Programa"]}\", \"#{row["Nome Programa"]}\", \"#{row["Linguagem Cidadã"]}\")")
+    		con.query("INSERT IGNORE INTO type_expenses(id, description) VALUES(\"#{row["Código Elemento Despesa"]}\", \"#{row["Nome Elemento Despesa"]}\")")
+    		con.query("INSERT IGNORE INTO companies(id, name) VALUES(\"#{row["Código Favorecido"]}\", \"#{row["Nome Favorecido"].gsub("\"", "\'")}\")")
+    		con.query("INSERT IGNORE INTO functions(id, description) VALUES(\"#{row["Código Função"]}\", \"#{row["Nome Função"]}\")")
+    		con.query("INSERT IGNORE INTO expenses(public_agency_id, document_number, value, payment_date, program_id, function_id, type_expense_id, company_id) \
+    			VALUES(\"#{row["Código Órgão"]}\", \"#{row["Número Documento"]}\", \"#{row["Valor"]}\", STR_TO_DATE('#{row["Data Pagamento"]}', '%d/%m/%Y'), \"#{row["Código Programa"]}\", \"#{row["Código Função"]}\", \"#{row["Código Elemento Despesa"]}\", \"#{row["Código Favorecido"]}\")")
 	
-		count = count+1
-		system "clear"
-		puts "#{((count/file_size)*100).round(2)}%"
-	end
+        	count = count+1
+        	system "clear"
 
-	create_preprocess_data_table(con)
+            puts "Processing file #{current_file} of #{total_files}"
+        	puts "#{((count/file_size)*100).round(2)}%"
+    	end
 
-rescue Mysql::Error => e
-    puts e.errno
-    puts e.error
-    
-ensure
-    con.close if con
+	      create_preprocess_data_table(con)
+
+        dump_file = csv_file.gsub(".csv", ".sql")
+        `mysqldump --no-create-info --replace --complete-insert -u root #{db_name} > #{dump_file}`
+        current_file = current_file+1
+
+    rescue Mysql2::Error => e
+        puts e.errno
+        puts e.error
+
+    ensure
+        con.close if con
+    end
 end
